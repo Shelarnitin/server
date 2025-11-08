4ea942edb704892ed6658418066f590977ae2950
Shelarnitin <shelarnitin399@gmail.com>
2025-11-08 22:21:31 +0530

    error middleware

---
+++ middleware/error.middleware.js
@@ -0,0 +1,22 @@
+export class ApiError extends Error {
+    constructor(message, statusCode){
+        super(message);
+        this.statusCode = statusCode
+        this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error'
+        this.isOperational = true; // optional
+
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+
+export const catchAsync = (fn) => {
+    return (req, res, next) => {
+        fn(req, res, next).catch(next)
+    }
+}
+
+// handle JWT error
+export const handleJWTError = () => {
+    new ApiError('Invalid token, Please log in again', 401)
+}
